bd5c94833d08c998747f9036637a945e74af9e2f
Jose Reis <jccreis@hotmail.com>
2021-06-21 17:09:00 +0100

    tradução completa e nome do jogo game.py

--- Pulaski_report.py
+++ Pulaski_report.py
@@ -1,11 +1,11 @@
 def game():
     import random
-    decide = ["right", "left"]
-    aproach = ["y","n"]
-    resposta = input("Queres jogar o jogo? (y/n)")
+    decide = ["direita", "esquerda"]
+    aproach = ["s","n"]
+    resposta = input("Queres jogar o jogo? (s/n)")
                   
     if resposta.lower() == 'y':
-        print("Benvindo ao cataclismo")
+        print("Bem-vindo ao cataclismo")
         start=True
         inventory=[]
             
@@ -17,9 +17,9 @@ def game():
         print("Estranho: Olá viajante, vejo que és novo nesta cidade.")
         print("Estranho: Bem-vindo, aqui é Pulaski. Mão encontrarás outra cidade no mundo mais interessada em proporcionar hospitalidade")
         print("Estranho: De momento, o importante é manter-te vivo, vejo que só tens 20HP. Não temas, isto irá ajudar na tua viagem.")
-        choice=input("Queres a minha ajuda?(y/n)")
+        choice=input("Queres a minha ajuda?(s/n)")
         
-    if choice.lower() == "y":
+    if choice.lower() == "s":
         choiceY1 = True
         print("Ainda bem que aceitas miudo")
         print("###--- Recebeste uma soqueira ---###")
@@ -28,43 +28,44 @@ def game():
         
     else:
         choiceY1=False
-        print("Cool ma, I hear ya. Here, ave this map of the city so if you get lost you have something to guide you at least")
-        print("Well, this seems to be the end")
-        print("###--- You have been given a Map ---###")
-        inventory.append("Map")
+        print("Tu é que sabes rapaz. Toma, fica com este mapa da cidade, no caso de te perderes, pelo menos")
+        print("Bem, parece que isto é o adeus")
+        print("###--- Recebeste um mapa ---###")
+        inventory.append("Mapa")
         
         
-    if choiceY1==False and "Map" in inventory:
-        print("your map tells you to take a left")
-        decide=input("do you take a left or right?") 
+    if choiceY1==False and "Mapa" in inventory:
+        print("Verificas o mapa e vês que tens que ir para a esquerda")
+        decide=input("vais para a direita ou esquerda?(direita/esquerda)") 
         
     elif choiceY1==True:
-        print("Cmon man, follow me to the park. There is something important there to show you - it will help you on your journey ")
+        print("Vamos lá rapaz, segue-me até ao parque. Há uma coisa importante para te mostrar lá e que te irá ajudar na tua viagem.")
         print("Oh, hold up, that there is dange dan, he likes to fight people with his mind and you should fight him")
-        aproach=input("do you apropach danger dan?(y/n)")
+        print("Oh, aguenta, está ali o Danger Dan. Ele gosta de lutar com pessoas com a mente e tu devias treinar com ele.")
+        aproach=input("Tu aproximas-te do Danger Dan?(s/n)")
     
-    if decide == "right":
-        print("you got in a car accident and now you are dead")
-        print("game over")   
+    if decide == "direita":
+        print("Tu tiveste um acidente de carro e morreste")
+        print("GAME OVER")   
         
-    elif decide == "left":
-        aproach1=input("You are at a park and someone wants to fight you, do you approach him?(y/n")
+    elif decide == "esquerda":
+        aproach1=input("You are at a park and someone wants to fight you, do you approach him?(s/n")
     
     
     """    para continuar
-    if aproach1.lower() == "y":
+    if aproach1.lower() == "s":
         print("") 
      """    
     if aproach =="n":
-        print("you are a coward")
+        print("És um cobarde.")
         print("game over")    
     
-    if aproach == "y":
-        print("***--- You are in a fight. Will you: ---***")
-        print("***--- A: punch him? Sucess: 85%")
-        print("***--- B: Kick him? Sucess: 33%")
-        print("***--- Use Brass Knucles? Sucess: 95%")
-        decision= input("What will be your choice: A,B,C?")
+    if aproach == "s":
+        print("***--- Estás numa luta. Tu vais?: ---***")
+        print("***--- A: Dar-lhe um soco? Sucesso: 85%")
+        print("***--- B: Dar-lhe um pontapé? Sucesso: 33%")
+        print("***--- Usar a Soqueira? Sucesso: 95%")
+        decision= input("O que vais escolher: A,B,C?")
         
         minPoint=1
         maxPoint=100
@@ -74,19 +75,19 @@ def game():
         ChC = (random.randint(minPoint, maxPoint))*.95
         
         if decision.upper() == "A" and ChA > 25:
-            print("You decide to punch him")
-            print("He his knocked out cold")
+            print("Tu decidiste dar-lhe um soco")
+            print("Ele aterrou no chão")
             
         elif decision.upper() == "B" and ChB > 25:
-            print("You decide to kick him")
-            print("He is knocked out cold")
+            print("Tu decidiste dar-lhe um pontapé")
+            print("Ele aterrou no chão")
             
-        elif decision.upper() == "C" and ChC > 25 and "Brass Knuckles" in inventory:
-            print("You use your Brass Knucles")
-            print("You beat hm pretty bad")
+        elif decision.upper() == "C" and ChC > 25 and "Soqueira" in inventory:
+            print("Tu usaste a Soqueira")
+            print("Deste-lhe uma enorme coça")
             
         else:
-            print("That didn't knot work, and you are knocked out cold")
+            print("Não conseguiste acertar e perdeste a luta")
             print("Game Over")
     
 game()
